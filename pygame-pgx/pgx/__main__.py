716b1009670a9ba5550f9c8e5c848f7dfa52fcb2
Carl Furtado <carlzfurtado@gmail.com>
2025-04-17 14:30:49 -0400

    final commit

--- pygame-pgx/pgx/__main__.py
+++ pygame-pgx/pgx/__main__.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import sys
 from .run import run
--- tests/testproj/scripts/main.py
+++ tests/testproj/scripts/main.py
@@ -1,5 +1,5 @@
+import time
 from pgx.api.annotations import *
-import pygame as pg
 import random
 
 def gen_rect_fill():
@@ -10,11 +10,17 @@ def gen_rect_fill():
 def pgx_update():
 	# print(app.get_element_by_id("wrapper").surf.get_size())
 
+	print(f"Seconds since last update: {time.time()-exports.last_update}")
 
 	app.get_element_by_id("rect").surf.fill(gen_rect_fill())
+
+	export("last_update", time.time())
 
 print(current_script)
 
 print(app)
 
+print(exports.myvar)
+
 exportfn(pgx_update)
+export("last_update", time.time())
